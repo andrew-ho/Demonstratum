e116c1c988e3401f72cdd0d3789d5a8dc9e69e1a
John <jakebritti@gmail.com>
2018-12-11 09:58:27 -0500

    made level manager be able to serialize multiple goals

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+	[HideInInspector]
 	public Vector2[] curGoal;
-	public Vector2[][] goals;
+	public Goal[] goals;
+
+	private void Start()
+	{
+		curGoal = goals[0].notes;
+	}
+
+	public void SetCurGoal(int i)
+	{
+		curGoal = goals[i].notes;
+	}
+}
+
+[System.Serializable]
+public class Goal
+{
+	public Vector2[] notes = new Vector2[3];
 }
