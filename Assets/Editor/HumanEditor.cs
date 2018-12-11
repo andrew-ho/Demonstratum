4efefa98fb01a5deacdef24679b89a65f6920c85
John <jakebritti@gmail.com>
2018-12-11 10:08:43 -0500

    can now break human line of sight during chase

--- Assets/Editor/HumanEditor.cs
+++ Assets/Editor/HumanEditor.cs
@@ -3,92 +3,98 @@ using UnityEditor;
 
 [CustomEditor(typeof(Human))]
 [CanEditMultipleObjects]
-public class HumanEditor : Editor {
-    SerializedProperty minWaitTime;
-    SerializedProperty maxWaitTime;
-    SerializedProperty searchRadius;
-    SerializedProperty viewAngle;
-    SerializedProperty viewDistance;
-    SerializedProperty sightLengthError;
-    SerializedProperty minLookTime;
-    SerializedProperty maxLookTime;
-    SerializedProperty minLookDir;
-    SerializedProperty maxLookDir;
-    SerializedProperty lookSpeed;
-    SerializedProperty lookCurve;
-    SerializedProperty state;
-    SerializedProperty attackDist;
-    SerializedProperty fireRate;
-    SerializedProperty damage;
-    SerializedProperty projectilePrefab;
+public class HumanEditor : Editor
+{
+	SerializedProperty minWaitTime;
+	SerializedProperty maxWaitTime;
+	SerializedProperty searchRadius;
+	SerializedProperty viewAngle;
+	SerializedProperty viewDistance;
+	SerializedProperty sightLengthError;
+	SerializedProperty minLookTime;
+	SerializedProperty maxLookTime;
+	SerializedProperty minLookDir;
+	SerializedProperty maxLookDir;
+	SerializedProperty lookSpeed;
+	SerializedProperty lookCurve;
+	SerializedProperty state;
+	SerializedProperty attackDist;
+	SerializedProperty fireRate;
+	SerializedProperty damage;
+	SerializedProperty projectilePrefab;
+	SerializedProperty chaseViewDistance;
 
-    void OnEnable()
-    {
-        searchRadius = serializedObject.FindProperty("searchRadius");
-        minWaitTime = serializedObject.FindProperty("minWaitTime");
-        maxWaitTime = serializedObject.FindProperty("maxWaitTime");
+	void OnEnable()
+	{
+		searchRadius = serializedObject.FindProperty("searchRadius");
+		minWaitTime = serializedObject.FindProperty("minWaitTime");
+		maxWaitTime = serializedObject.FindProperty("maxWaitTime");
 
-        viewAngle = serializedObject.FindProperty("viewAngle");
-        viewDistance = serializedObject.FindProperty("viewDistance");
+		viewAngle = serializedObject.FindProperty("viewAngle");
+		viewDistance = serializedObject.FindProperty("viewDistance");
 
-        minLookTime = serializedObject.FindProperty("minLookTime");
-        maxLookTime = serializedObject.FindProperty("maxLookTime");
-        minLookDir = serializedObject.FindProperty("minLookDir");
-        maxLookDir = serializedObject.FindProperty("maxLookDir");
-        lookSpeed = serializedObject.FindProperty("lookSpeed");
-        lookCurve = serializedObject.FindProperty("lookCurve");
-        state = serializedObject.FindProperty("state");
+		minLookTime = serializedObject.FindProperty("minLookTime");
+		maxLookTime = serializedObject.FindProperty("maxLookTime");
+		minLookDir = serializedObject.FindProperty("minLookDir");
+		maxLookDir = serializedObject.FindProperty("maxLookDir");
+		lookSpeed = serializedObject.FindProperty("lookSpeed");
+		lookCurve = serializedObject.FindProperty("lookCurve");
+		state = serializedObject.FindProperty("state");
 
-        attackDist = serializedObject.FindProperty("attackDist");
-        fireRate = serializedObject.FindProperty("fireRate");
-        damage = serializedObject.FindProperty("damage");
-        projectilePrefab = serializedObject.FindProperty("projectilePrefab");
-        SceneView.onSceneGUIDelegate += OnSceneViewGUI;
-    }
+		attackDist = serializedObject.FindProperty("attackDist");
+		fireRate = serializedObject.FindProperty("fireRate");
+		damage = serializedObject.FindProperty("damage");
+		projectilePrefab = serializedObject.FindProperty("projectilePrefab");
+		chaseViewDistance = serializedObject.FindProperty("chaseViewDistance");
+		SceneView.onSceneGUIDelegate += OnSceneViewGUI;
+	}
 
-    void OnDisable()
-    {
-        SceneView.onSceneGUIDelegate -= OnSceneViewGUI;
-    }
-    
-    public override void OnInspectorGUI() {
-        serializedObject.Update();
-        EditorGUILayout.LabelField("Searching", EditorStyles.boldLabel);
-        EditorGUILayout.PropertyField(state);
-        EditorGUILayout.PropertyField(searchRadius);
-        MinMax("Wait Time:", minWaitTime, maxWaitTime);
-        EditorGUILayout.LabelField("Random Looking", EditorStyles.boldLabel);
-        MinMax("Look Time:", minLookTime, maxLookTime);
-        MinMax("Look Dir:", minLookDir, maxLookDir);
-        EditorGUILayout.PropertyField(lookSpeed);
-        EditorGUILayout.PropertyField(lookCurve);
-        EditorGUILayout.LabelField("Detection", EditorStyles.boldLabel);
-        EditorGUILayout.PropertyField(viewAngle);
-        EditorGUILayout.PropertyField(viewDistance);
-        EditorGUILayout.PropertyField(attackDist);
-        EditorGUILayout.PropertyField(fireRate);
-        EditorGUILayout.PropertyField(damage);
-        EditorGUILayout.PropertyField(projectilePrefab);
-        serializedObject.ApplyModifiedProperties();
-    }
+	void OnDisable()
+	{
+		SceneView.onSceneGUIDelegate -= OnSceneViewGUI;
+	}
 
-    private void OnSceneViewGUI(SceneView sv)
-    {
-        Handles.color = new Color(1,0,0,1);
-        Human hu = target as Human;
-        Handles.DrawWireDisc(hu.transform.position, hu.transform.up, searchRadius.floatValue);
-        Handles.color = new Color(0,1,0,0.2f);
-        Handles.DrawSolidArc(hu.transform.position, hu.transform.up, hu.transform.forward, viewAngle.floatValue, viewDistance.floatValue);
-        Handles.DrawSolidArc(hu.transform.position, hu.transform.up, hu.transform.forward, -viewAngle.floatValue, viewDistance.floatValue);
-    }
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+		EditorGUILayout.LabelField("Searching", EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField(state);
+		EditorGUILayout.PropertyField(searchRadius);
+		MinMax("Wait Time:", minWaitTime, maxWaitTime);
+		EditorGUILayout.LabelField("Random Looking", EditorStyles.boldLabel);
+		MinMax("Look Time:", minLookTime, maxLookTime);
+		MinMax("Look Dir:", minLookDir, maxLookDir);
+		EditorGUILayout.PropertyField(lookSpeed);
+		EditorGUILayout.PropertyField(lookCurve);
+		EditorGUILayout.LabelField("Detection", EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField(viewAngle);
+		EditorGUILayout.PropertyField(viewDistance);
+		EditorGUILayout.PropertyField(attackDist);
+		EditorGUILayout.PropertyField(fireRate);
+		EditorGUILayout.PropertyField(damage);
+		EditorGUILayout.PropertyField(projectilePrefab);
+		EditorGUILayout.PropertyField(chaseViewDistance);
+		serializedObject.ApplyModifiedProperties();
+	}
 
-    private void MinMax(string label, SerializedProperty min, SerializedProperty max) {
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField(label);
-        GUILayout.Label("Min");
-        min.floatValue = EditorGUILayout.FloatField(min.floatValue);
-        GUILayout.Label("Max");
-        max.floatValue = EditorGUILayout.FloatField(max.floatValue);
-        EditorGUILayout.EndHorizontal();
-    }
+	private void OnSceneViewGUI(SceneView sv)
+	{
+		Handles.color = new Color(1, 0, 0, 1);
+		Human hu = target as Human;
+		Handles.DrawWireDisc(hu.transform.position, hu.transform.up, searchRadius.floatValue);
+		Handles.color = new Color(0, 1, 0, 0.2f);
+		Handles.DrawSolidArc(hu.transform.position, hu.transform.up, hu.transform.forward, viewAngle.floatValue, viewDistance.floatValue);
+		Handles.DrawSolidArc(hu.transform.position, hu.transform.up, hu.transform.forward, -viewAngle.floatValue, viewDistance.floatValue);
+	}
+
+	private void MinMax(string label, SerializedProperty min, SerializedProperty max)
+	{
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField(label);
+		GUILayout.Label("Min");
+		min.floatValue = EditorGUILayout.FloatField(min.floatValue);
+		GUILayout.Label("Max");
+		max.floatValue = EditorGUILayout.FloatField(max.floatValue);
+		EditorGUILayout.EndHorizontal();
+	}
 }
--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -7,170 +7,208 @@ using UnityEditor;
 public class Human : MonoBehaviour
 {
 
-    public float minWaitTime;
-    public float maxWaitTime;
-    public float minLookTime;
-    public float maxLookTime;
-    public float minLookDir;
-    public float maxLookDir;
-    [Range(0.0f, 50.0f)] public float searchRadius;
-    [Range(0.0f, 2.0f)] public float moveError;
-    public float lookSpeed;
-    public AnimationCurve lookCurve;
-    [Range(0.0f, 50.0f)]
-    public float viewDistance;
-    [Range(0.0f, 180.0f)]
-    public float viewAngle;
-    public float sightLengthError;
-    [Range(0.0f, 50.0f)]
-    public float attackDist;
-    public float fireRate;
-    public float damage;
-    public GameObject projectilePrefab;
+	public float minWaitTime;
+	public float maxWaitTime;
+	public float minLookTime;
+	public float maxLookTime;
+	public float minLookDir;
+	public float maxLookDir;
+	[Range(0.0f, 50.0f)] public float searchRadius;
+	[Range(0.0f, 2.0f)] public float moveError;
+	public float lookSpeed;
+	public AnimationCurve lookCurve;
+	[Range(0.0f, 50.0f)]
+	public float viewDistance;
+	[Range(0.0f, 180.0f)]
+	public float viewAngle;
+	public float sightLengthError;
+	[Range(0.0f, 50.0f)]
+	public float attackDist;
+	public float fireRate;
+	public float damage;
+	public GameObject projectilePrefab;
+	public float chaseViewDistance;
 
-    private NavMeshAgent agent;
-    public HumanState state;
-    private Vector3 searchPosition;
-    private float waitTimer;
-    private float lookTimer;
-    private float sightTimer;
-    private bool moving;
-    private float lookTarget;
-    private Light sight;
-    private float fireTimer;
+	private NavMeshAgent agent;
+	public HumanState state;
+	private Vector3 searchPosition;
+	private float waitTimer;
+	private float lookTimer;
+	private float sightTimer;
+	private bool moving;
+	private float lookTarget;
+	private Light sight;
+	private float fireTimer;
+	private Vector3 lastPlayerPos;
 
-    private IEnumerator lookRoutine;
+	private IEnumerator lookRoutine;
 
-    // void OnInspectorGUI()
-    // {
-    //     EditorGUILayout.LabelField("Wait Time");
-    //     EditorGUILayout.MinMaxSlider(ref minWaitTime, ref maxWaitTime, 0, 30);
-    //     EditorGUILayout.LabelField("Look Time");
-    //     EditorGUILayout.MinMaxSlider(ref minLookTime, ref maxLookTime, 0, 5);
-    //     EditorGUILayout.LabelField("Look Time");
-    //     EditorGUILayout.MinMaxSlider(ref minLookTime, ref maxLookTime, -360, 360);
-    // }
-    
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        sight = GetComponentInChildren<Light>();
-        searchPosition = transform.position;
-        lookRoutine = look();
-        sightTimer = sightLengthError;
-    }
+	// void OnInspectorGUI()
+	// {
+	//     EditorGUILayout.LabelField("Wait Time");
+	//     EditorGUILayout.MinMaxSlider(ref minWaitTime, ref maxWaitTime, 0, 30);
+	//     EditorGUILayout.LabelField("Look Time");
+	//     EditorGUILayout.MinMaxSlider(ref minLookTime, ref maxLookTime, 0, 5);
+	//     EditorGUILayout.LabelField("Look Time");
+	//     EditorGUILayout.MinMaxSlider(ref minLookTime, ref maxLookTime, -360, 360);
+	// }
 
-    void Update()
-    {
-        switch(state) {
-            case HumanState.Idle:
-                break;
-            case HumanState.Patrole:
-                patrole();
-                break;
-            case HumanState.Chase:
-                chase();
-                break;
-            case HumanState.Attack:
-                attack();
-                break;
-        }
-    }
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		sight = GetComponentInChildren<Light>();
+		searchPosition = transform.position;
+		lookRoutine = look();
+		sightTimer = sightLengthError;
+	}
 
-    void patrole() {
-        if (playerInSight()) {
-            sightTimer += Time.deltaTime;
-            if (sightTimer > sightLengthError) {
-                state = HumanState.Chase;
-                sightTimer = 0;
-            }
-            // Debug.DrawLine(sight.transform.position,
-            //     GameManager.instance.player.transform.position, Color.green);
-        } else {
-            sightTimer = 0;
-            Debug.DrawLine(sight.transform.position,
-                GameManager.instance.player.transform.position, Color.red);
-            sight.color = new Color(1, 0.8628919f, 0, 1);
-        }
+	void Update()
+	{
+		switch (state)
+		{
+			case HumanState.Idle:
+				break;
+			case HumanState.Patrole:
+				patrole();
+				break;
+			case HumanState.Chase:
+				chase();
+				break;
+			case HumanState.Attack:
+				attack();
+				break;
+		}
+	}
 
-        if (moving && Vector3.Distance(transform.position, agent.destination) < moveError) {
-            moving = false;
-        } else {
-            waitTimer -= Time.deltaTime;
-            lookTimer -= Time.deltaTime;
-            if (waitTimer <= 0) {
-                StopCoroutine(lookRoutine);
-                setRandomTarget();
-                moving = true;
-                waitTimer = Random.Range(minWaitTime, maxWaitTime);
-            }
-            if (lookTimer <= 0) {
-                StopCoroutine(lookRoutine);
-                lookRoutine = look();
-                lookTarget += Random.Range(minLookDir, maxLookDir);
-                lookTimer = Random.Range(minLookTime, maxLookTime);
-                StartCoroutine(lookRoutine);
-            }
-        }
-    }
+	void patrole()
+	{
+		if (playerInSight())
+		{
+			sightTimer += Time.deltaTime;
+			if (sightTimer > sightLengthError)
+			{
+				state = HumanState.Chase;
+				sightTimer = 0;
+			}
+			// Debug.DrawLine(sight.transform.position,
+			//     GameManager.instance.player.transform.position, Color.green);
+		}
+		else
+		{
+			sightTimer = 0;
+			Debug.DrawLine(sight.transform.position,
+					GameManager.instance.player.transform.position, Color.red);
+			sight.color = new Color(1, 0.8628919f, 0, 1);
+		}
 
-    void chase() {
-        agent.SetDestination(GameManager.instance.player.transform.position);
-        // state = HumanState.Patrole;
-        sight.color = Color.red;
-        if (Vector3.Distance(GameManager.instance.player.transform.position, transform.position) < attackDist) {
-            state = HumanState.Attack;
-        }
-    }
+		if (moving && Vector3.Distance(transform.position, agent.destination) < moveError)
+		{
+			moving = false;
+		}
+		else
+		{
+			waitTimer -= Time.deltaTime;
+			lookTimer -= Time.deltaTime;
+			if (waitTimer <= 0)
+			{
+				StopCoroutine(lookRoutine);
+				setRandomTarget();
+				moving = true;
+				waitTimer = Random.Range(minWaitTime, maxWaitTime);
+			}
+			if (lookTimer <= 0)
+			{
+				StopCoroutine(lookRoutine);
+				lookRoutine = look();
+				lookTarget += Random.Range(minLookDir, maxLookDir);
+				lookTimer = Random.Range(minLookTime, maxLookTime);
+				StartCoroutine(lookRoutine);
+			}
+		}
+	}
 
-    private bool playerInSight() {
-        Vector3 toPlayer = GameManager.instance.player.transform.position - sight.transform.position;
-        float angle = Vector2.Angle(new Vector2(toPlayer.x, toPlayer.z), 
-            new Vector2(transform.forward.x, transform.forward.z));
-        bool inSight = false;
-        if (angle < viewAngle && toPlayer.magnitude < viewDistance) {
-            RaycastHit hit;
-            if (Physics.Raycast(sight.transform.position, toPlayer, out hit, viewDistance)) {
-                inSight = hit.collider.gameObject.GetComponent<Player>() != null;
-            }
-        }
-        return inSight;
-    }
+	void chase()
+	{
+		Vector3 toPlayer = GameManager.instance.player.transform.position - sight.transform.position;
+		if (toPlayer.magnitude <= chaseViewDistance)
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(sight.transform.position, toPlayer, out hit, viewDistance))
+			{
+				if (hit.collider.gameObject.GetComponent<Player>() != null)
+					lastPlayerPos = GameManager.instance.player.transform.position;
+				else
+				{
+					searchPosition = lastPlayerPos;
+					state = HumanState.Patrole;
+					return;
+				}
+			}
+		}
+		agent.SetDestination(lastPlayerPos);
+		sight.color = Color.red;
+		if (Vector3.Distance(GameManager.instance.player.transform.position, transform.position) < attackDist)
+		{
+			state = HumanState.Attack;
+		}
+	}
 
-    void attack() {
+	private bool playerInSight()
+	{
+		Vector3 toPlayer = GameManager.instance.player.transform.position - sight.transform.position;
+		float angle = Vector2.Angle(new Vector2(toPlayer.x, toPlayer.z),
+				new Vector2(transform.forward.x, transform.forward.z));
+		bool inSight = false;
+		if (angle < viewAngle && toPlayer.magnitude < viewDistance)
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(sight.transform.position, toPlayer, out hit, viewDistance))
+			{
+				inSight = hit.collider.gameObject.GetComponent<Player>() != null;
+			}
+		}
+		return inSight;
+	}
 
-    }
+	void attack()
+	{
 
-    IEnumerator look() {
-        float time = 0;
-        float perc = 0;
-        float lastTime = Time.realtimeSinceStartup;
-        Quaternion curLook = transform.rotation;
-        do
-        {
-          time += Time.realtimeSinceStartup - lastTime;
-          lastTime = Time.realtimeSinceStartup;
-          perc = Mathf.Clamp01(time / lookSpeed);
-          
-          transform.rotation = Quaternion.Lerp(curLook, Quaternion.Euler(curLook.eulerAngles.x, lookTarget, curLook.eulerAngles.z), lookCurve.Evaluate(perc));
-          yield return null;
-        } while (perc < 1);
-    }
+	}
 
-    void setRandomTarget() {
-        Vector3 finalPosition = Vector3.zero;
-        for (int i = 0; i < 1000 && finalPosition == Vector3.zero; i++) {
-            Vector3 randomDirection = searchPosition + Random.insideUnitSphere * searchRadius;
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomDirection, out hit, searchRadius, 1)) {
-                finalPosition = hit.position;            
-            }
-        }
-        agent.SetDestination(finalPosition);
-    }
+	IEnumerator look()
+	{
+		float time = 0;
+		float perc = 0;
+		float lastTime = Time.realtimeSinceStartup;
+		Quaternion curLook = transform.rotation;
+		do
+		{
+			time += Time.realtimeSinceStartup - lastTime;
+			lastTime = Time.realtimeSinceStartup;
+			perc = Mathf.Clamp01(time / lookSpeed);
+
+			transform.rotation = Quaternion.Lerp(curLook, Quaternion.Euler(curLook.eulerAngles.x, lookTarget, curLook.eulerAngles.z), lookCurve.Evaluate(perc));
+			yield return null;
+		} while (perc < 1);
+	}
+
+	void setRandomTarget()
+	{
+		Vector3 finalPosition = Vector3.zero;
+		for (int i = 0; i < 1000 && finalPosition == Vector3.zero; i++)
+		{
+			Vector3 randomDirection = searchPosition + Random.insideUnitSphere * searchRadius;
+			NavMeshHit hit;
+			if (NavMesh.SamplePosition(randomDirection, out hit, searchRadius, 1))
+			{
+				finalPosition = hit.position;
+			}
+		}
+		agent.SetDestination(finalPosition);
+	}
 }
 
 public enum HumanState
 {
-    Idle, Patrole, Chase, Attack
+	Idle, Patrole, Chase, Attack
 }
