12a6e0047265cbf7b7f3f6482d3d1b3da2aa65d3
RoboticWater <jakebritti@gmail.com>
2019-02-01 20:35:23 -0500

    started changing voice box to horizonal harmony creator + tweaked movement gravity

--- Assets/Scripts/Cam/Effects/RetroSize.cs
+++ Assets/Scripts/Cam/Effects/RetroSize.cs
@@ -7,29 +7,13 @@ namespace Assets.Scripts.Cam.Effects {
 	[AddComponentMenu("Image Effects/Custom/Retro Size")]
 	public class RetroSize : MonoBehaviour {
 		[Header("Resolution")]
-		public int horizontalResolution = 160;
 		public int verticalResolution = 144;
-//		public float ratioForce = 1f;
+		public float aspectRatio = 1.333f;
+		int horizontalResolution {
+			get { return (int) (verticalResolution * aspectRatio); }
+		}
 
 		public void OnRenderImage(RenderTexture src, RenderTexture dest) {
-			horizontalResolution = Mathf.Clamp(horizontalResolution, 1, 2048);
-			verticalResolution = Mathf.Clamp(verticalResolution, 1, 2048);
-
-//			float w = Screen.currentResolution.width;
-//			float h = Screen.currentResolution.height;
-//
-//			float rw = w / horizontalResolution;
-//			float rh = h / verticalResolution;
-//
-//			float r = Math.Min(rw, rh) * ratioForce;
-//
-//			RenderTexture scaled = RenderTexture.GetTemporary((int) (horizontalResolution * r), (int) (verticalResolution * r));
-
-			// For GBJam, you probably don't want the above
-			// + I don't think it's even working properly, I never really tested it
-			// It was meant to keep the downsampling consistent no matter what was the window size
-			// so that if you put the game in fullscreen, it wouldn't be very pixelated, it would actually
-			// give you more resolution by using some sort of ratio or w.e
 
 			RenderTexture scaled = RenderTexture.GetTemporary(horizontalResolution, verticalResolution);
 			scaled.filterMode = FilterMode.Point;
