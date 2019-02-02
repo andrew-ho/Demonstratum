5f9fa1a0a0c9d28c768a6e8fce0b987af1381b40
RoboticWater <jakebritti@gmail.com>
2019-02-01 23:34:56 -0500

    harmony box interaction framework

---
+++ Assets/Scripts/Utils/Math.cs
@@ -0,0 +1,14 @@
+﻿namespace Utils {
+    public static class Math
+    {
+        public static float Map(float value, float from, float to, float onFrom, float onTo) {
+            if (value <= from) {
+                return onFrom;
+            } else if(value >= to) {
+                return onTo;
+            } else {
+                return (onTo - onFrom) * ((value - from) / (to - from)) + onFrom;
+            }
+        }
+    }
+}
